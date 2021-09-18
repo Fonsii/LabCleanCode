adf8662fc687daf50c8804861c2496468af9ddb7
Fonsii <lualfonso14200174@gmail.com>
2021-09-18 17:57:29 -0600

    Initial and Final Commit

---
+++ LabCleanCode/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LabCleanCode
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(Roman.To(3999));
+            // Hangman.PlayHangman();
+            // NumberGuessing.Play();
+            // Letters.Count();
+        }
+    }
+}
---
+++ LabCleanCode/Roman.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabCleanCode
+{
+    class Roman
+    {
+        public static readonly Dictionary<char, int> RomanNumberDictionary;
+        public static readonly Dictionary<int, string> NumberRomanDictionary;
+
+        static Roman()
+        {
+            RomanNumberDictionary = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 },
+        };
+
+            NumberRomanDictionary = new Dictionary<int, string>
+        {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" },
+        };
+        }
+
+
+        public static string To(int number)
+        {
+            var roman = new StringBuilder();
+
+            foreach (var item in NumberRomanDictionary)
+            {
+                while (number >= item.Key)
+                {
+                    roman.Append(item.Value);
+                    number -= item.Key;
+                }
+            }
+
+            return roman.ToString();
+        }
+
+    }
+}
